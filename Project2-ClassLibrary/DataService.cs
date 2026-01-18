a14b3a1451c12f41396738329600025bf0398779
Kavya9935 <154084227+Kavya9935@users.noreply.github.com>
2026-01-18 17:03:11 -0600

    Create DataService.cs

---
+++ Project2-ClassLibrary/DataService.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Data.SqlClient;
+
+namespace Project2_ClassLibrary
+{
+    public class DataService
+    {
+        private readonly string _connectionString;
+        
+        public DataService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        
+        public void TestConnection()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Connection successful!");
+            }
+        }
+    }
+}
